205c21a644d449b2e4d2bdeacd290243690d10af
CN-litai <2083645366@qq.com>
2022-08-08 18:13:39 +0800

    feat: bill update

--- app/router.js
+++ app/router.js
@@ -15,4 +15,8 @@ module.exports = app => {
   router.get('/api/user/getUserByName', _jwt, controller.user.getUserInfo);
   router.post('/api/user/editUserInfo', _jwt, controller.user.updateUserInfo);
   router.post('/api/bill/add', _jwt, controller.bill.add); // 添加账单
+  router.get('/api/bill/list', _jwt, controller.bill.list); // 获取账单列表
+  router.get('/api/bill/detail', _jwt, controller.bill.detail); // 获取详情
+  router.post('/api/bill/update', _jwt, controller.bill.update); // 账单更新
+  router.post('/api/bill/delete', _jwt, controller.bill.delete); // 删除账单
 };
--- app/service/bill.js
+++ app/service/bill.js
@@ -13,6 +13,58 @@ class BillService extends Service {
       return null;
     }
   }
+  async list(id) {
+    const { ctx, app } = this;
+    const QUERY_STR = 'id, pay_type, amount, date, type_id, type_name, remark';
+    const sql = `select ${QUERY_STR} from bill where user_id = ${id}`;
+    try {
+      return await app.mysql.query(sql);
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  async detail(id, user_id) {
+    const { ctx, app } = this;
+    try {
+      const result = await app.mysql.get('bill', { id, user_id });
+      return result;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  async update(params) {
+    const { ctx, app } = this;
+    try {
+      let result = await app.mysql.update('bill', {
+        ...params
+      }, {
+        id: params.id,
+        user_id: params.user_id
+      });
+      return result;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  async delete(id, user_id) {
+    const { ctx, app } = this;
+    try {
+      let result = await app.mysql.delete('bill', {
+        id: id,
+        user_id: user_id
+      });
+      return result;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
 }
 
 module.exports = BillService;
